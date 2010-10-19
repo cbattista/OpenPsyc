cad6e7ff5745463288a520e4345e35b72e3a0dcc
cogdev <cogdev@6232ac.ssc.uwo.ca>
2010-10-19 16:13:52 -0400

    new adds to that dot arrays with multiple colors can be created (all overlaid on some image)

--- stimCreators/DotMaster.py
+++ stimCreators/DotMaster.py
@@ -6,69 +6,71 @@ import copy
 
 #HELPER FUNCS
 def circleArea(radius):
-    return math.pi * (radius ** 2)
+	return math.pi * (radius ** 2)
 
 def circleRadius(area):
-    return (area / math.pi) ** 0.5
+	return (area / math.pi) ** 0.5
 
 #make a left/right dot array stimulus from two groups of bounding boxes
 def makeStimulus(name, dots1, size, bgcolor, color, dots2=[], dpi=(96, 96)):
-    if dots2:
-        image = Image.new("RGB", [size[0] * 2, size[1]], bgcolor)    
-    else:
-        image = Image.new("RGB", size, bgcolor)
-    
-    draw = ImageDraw.Draw(image)
+	if dots2:
+		image = Image.new("RGB", [size[0] * 2, size[1]], bgcolor)    
+	else:
+		image = Image.new("RGB", size, bgcolor)
 
-    for d1 in dots1:
-        #draw the dots on the left
-        box1 = [d1[0] - d1[2], d1[1] - d1[2], d1[0] + d1[2], d1[1] + d1[2]]
-        draw.ellipse(box1, fill = color)
-            
-    if dots2:
-        for d2 in dots2:
-            #draw the dots on the right
-            box2 = [d2[0] - d2[2] + size[0], d2[1] - d2[2], d2[0] + d2[2] + size[0], d2[1] + d2[2]]
-            draw.ellipse(box2, fill = color)
-        
-        draw.line([size[0], 0, size[0], size[1]], fill = color, width = 2)
-    del draw
-    #image = image.filter(ImageFilter.BLUR)
+	draw = ImageDraw.Draw(image)
 
-    image.save("%s_NONSYM.bmp" % name, "BMP", dpi=dpi)
+	for d1 in dots1:
+		#draw the dots on the left
+		box1 = [d1[0] - d1[2], d1[1] - d1[2], d1[0] + d1[2], d1[1] + d1[2]]
+		draw.ellipse(box1, fill = color)
+
+	if dots2:
+		for d2 in dots2:
+			#draw the dots on the right
+			box2 = [d2[0] - d2[2] + size[0], d2[1] - d2[2], d2[0] + d2[2] + size[0], d2[1] + d2[2]]
+			draw.ellipse(box2, fill = color)
+
+		draw.line([size[0], 0, size[0], size[1]], fill = color, width = 2)
+	del draw
+	#image = image.filter(ImageFilter.BLUR)
+
+	image.save("%s_NONSYM.bmp" % name, "BMP", dpi=dpi)
 
 def makeSymStimulus(name, n1, size, bgcolor, color,  n2=[], dpi = (96,96)):
-    #now let us make the image with the number in it
-    if n2:
-        image = Image.new("RGB", [size[0] * 2, size[1]], bgcolor)
-    else:
-        image = Image.new("RGB", size, bgcolor)
-    draw = ImageDraw.Draw(image)
+	#now let us make the image with the number in it
+	if n2:
+		image = Image.new("RGB", [size[0] * 2, size[1]], bgcolor)
+	else:
+		image = Image.new("RGB", size, bgcolor)
+	draw = ImageDraw.Draw(image)
 
-    font = ImageFont.truetype("arial.ttf", 128)
+	font = ImageFont.truetype("arial.ttf", 128)
 
 
-    #left text
-    text = str(len(n1))
-    fontsize = font.getsize(text)
-    draw.text((size[0]/2 - fontsize[0]/2 , size[1] /2 - fontsize[1]/2), text, fill = color, font = font)    
-    #right text
-    if n2:
-        text = str(len(n2))
-        fontsize = font.getsize(text)
-        draw.text((size[0]/2 - fontsize[0]/2 + size[0], size[1]/2 - fontsize[1]/2), text, fill = color, font = font)
-        
-        draw.line([size[0], 0, size[0], size[1]], fill = color, width = 2)
-    del draw
-    image.save("%s_SYM.bmp" % name, "BMP", dpi=dpi)
+	#left text
+	text = str(len(n1))
+	fontsize = font.getsize(text)
+	draw.text((size[0]/2 - fontsize[0]/2 , size[1] /2 - fontsize[1]/2), text, fill = color, font = font)    
+	#right text
+	if n2:
+		text = str(len(n2))
+		fontsize = font.getsize(text)
+		draw.text((size[0]/2 - fontsize[0]/2 + size[0], size[1]/2 - fontsize[1]/2), text, fill = color, font = font)
+
+		draw.line([size[0], 0, size[0], size[1]], fill = color, width = 2)
+	del draw
+	image.save("%s_SYM.bmp" % name, "BMP", dpi=dpi)
    
 class DotMaster:
-	def __init__(self, box, area, density=5, separation=150):
+	def __init__(self, box, area, density=5, separation=10, colors =[[255, 255, 255]], bgcolor = [0,0,0]):
 		self.box = box
 		self.area = area
 		self.density = density
 		self.separation = separation
 		self.dotArea = box[0] * box[1] * area
+		self.colors = colors
+		self.bgcolor = bgcolor
 
 
 	def dotSolver(self, n, MIN=.25, MAX=.75):
@@ -82,22 +84,38 @@ class DotMaster:
 		#5 - repeat until only 1 dot is left
 		#6 - make the last dot the necessary size so the area works out
 
-		print self.dotArea, n
 		avg = self.dotArea / n
 		operations = [-1, 1]
 		myAreas = []
 		for i in range(n-1):
-			print MIN, MAX
 			num = random.uniform(MIN, MAX)
 			operation = random.choice(operations)
 			myAreas.append(avg + (operation * num * avg))
-		    
+
 		total = sum(myAreas)
 		diff = abs(area - total + avg)
 		myAreas.append(avg + diff)
 		return myAreas
 
-	def dotArranger(self, n):
+	def generateLists(self, ns):
+		#generates the area lists, depending on the ns parameters
+		areaList = []
+		sepDots = []
+		if type(ns) == int:
+			areaList = self.dotSolver(n)
+
+		elif type(ns) == list:
+			for n in ns:
+				areas = self.dotSolver(n)
+				sepDots.append(areas)
+				areaList += areas
+
+		else:
+			print "Just what the hell do you think you're doing"
+
+		return areaList, sepDots
+
+	def dotArranger(self, ns):
 		#1 - place a dot box in a random location which does not overlap the edges
 		#2 - place a dot in a random location which does not overlap the edges or any other dot boxes
 		#3 - repeat until no dots are left
@@ -105,117 +123,104 @@ class DotMaster:
 		goodList = 0
 		breaks = 0
 
-		areaList = self.dotSolver(n)
+		areaList, sepDots = self.generateLists(ns)
 
-		if n != 1:
+		if len(areaList) != 1:
 
-		    while not goodList:
-		        dotBoxes = []
-		        count = 1
-		        dotAreas = copy.deepcopy(areaList)
-		        while len(dotAreas):
-		            a = dotAreas[-1]
-		            r = int(circleRadius(a))
-		            d = int(r * 2)
-		            quit = 0
-		            reps = 1
+			while not goodList:
+				dotBoxes = []
+				count = 1
+				dotAreas = copy.deepcopy(areaList)
+				while len(dotAreas):
+					a = dotAreas[-1]
+					r = int(circleRadius(a))
+					d = int(r * 2)
+					quit = 0
+					reps = 1
 
-		            #if we've broken the cycle more than 10 times, we should regenerate the area list, 'cause this obviously ain't workin'
-		            if breaks > 9:
-		                areaList = dotSolver(n)
-		                #print "regenning area list"
-		                breaks = 0
-		                dotBoxes = []
-		                dotAreas = copy.deepcopy(areaList)
+					#if we've broken the cycle more than 10 times, we should regenerate the area list, 'cause this obviously ain't workin'
+					if breaks > 9:
+						areaList, sepDots = self.generateLists(ns)
+						#print "regenning area list"
+						breaks = 0
+						dotBoxes = []
+						dotAreas = copy.deepcopy(areaList)
 
-		            while not quit:
-		                reps = reps + 1
-		                #print reps
-		                #if we've tried this too many times, restart the process...
-		                if reps > 100000:
-		                    #delete the list of boxes
-		                    #put the area back into the list of areas
-		                    dotAreas = copy.deepcopy(areaList)
-		                    #and break the loop
-		                    #print "(%s)break, n = %s" % (breaks, len(dotBoxes))
-		                    breaks = breaks + 1
-		                    dotBoxes = []
-		                    quit = 1
+					while not quit:
+						reps = reps + 1
+						#print reps
+						#if we've tried this too many times, restart the process...
+						if reps > 100000:
+							#delete the list of boxes
+							#put the area back into the list of areas
+							dotAreas = copy.deepcopy(areaList)
+							#and break the loop
+							#print "(%s)break, n = %s" % (breaks, len(dotBoxes))
+							breaks = breaks + 1
+							dotBoxes = []
+							quit = 1
 
-		                x = int(random.uniform(r + self.density, self.box[0] - r - self.density))
-		                y = int(random.uniform(r + self.density, self.box[1] - r - self.density))
-		                
-		                dotBox = (x, y, r)
-		                
-		                #if there are no dots on the screen, place the current dot on the screen and proceed to the next placement
-		                if count == 1:
-		                    dotBoxes.append(dotBox)
-		                    dotAreas.pop()
-		                    quit = 1
-		                #otherwise check against the existing list of dots
-		                else:
-		                    bad = 0
-		                    #print "Checking through dot list: %s" % dotBoxes
-		                    for box in dotBoxes:
-		                        minRadius = r + self.box[2] + 5 + separation
-		                        x2 = self.box[0]
-		                        y2 = self.box[1]        
-		                        
-		                        a = abs(x - x2)
-		                        b = abs(y - y2)
-		                        
-		                        cSquare = a**2 + b**2
-		                        c = (cSquare ** 0.5)
-		                        
-		                        if c < minRadius:
-		                            bad = 1
-		                                        
-		                    if not bad:
-		                        dotBoxes.append(dotBox)
-		                        goodList = 1
-		                        dotAreas.pop()
-		                        quit = 1
-		            count = count + 1
-		        #print "SUCCESS: %s" % dotBoxes
-		    #print len(dotBoxes), dotBoxes
+						x = int(random.uniform(r + self.density, self.box[0] - r - self.density))
+						y = int(random.uniform(r + self.density, self.box[1] - r - self.density))
+	
+						dotBox = [x, y, r, a]
+	
+						#if there are no dots on the screen, place the current dot on the screen and proceed to the next placement
+						if count == 1:
+							dotBoxes.append(dotBox)
+							dotAreas.pop()
+							quit = 1
+						#otherwise check against the existing list of dots
+						else:
+							bad = 0
+							for box in dotBoxes:
+								minRadius = r + box[2] + 5 + self.separation
+								x2 = box[0]
+								y2 = box[1]        
+					
+								ax = abs(x - x2)
+								by = abs(y - y2)
+					
+								cSquare = ax**2 + by**2
+								c = (cSquare ** 0.5)
+					
+								if c < minRadius:
+									bad = 1
+											    
+							if not bad:
+								dotBoxes.append(dotBox)
+								goodList = 1
+								dotAreas.pop()
+								quit = 1
+					count = count + 1
+				#print "SUCCESS: %s" % dotBoxes
+			#print len(dotBoxes), dotBoxes
 		else:
-		    a = areaList[0]
-		    r = int(circleRadius(a))
+			a = areaList[0]
+			r = int(circleRadius(a))
 
-		    x = int(random.uniform(r + self.density, self.bounds[0] - r - self.density))
-		    y = int(random.uniform(r + self.density, self.bounds[1] - r - self.density))
-		    dotBoxes = [(x, y, r, a)]
-    	
-		return dotBoxes
+			x = int(random.uniform(r + self.density, self.bounds[0] - r - self.density))
+			y = int(random.uniform(r + self.density, self.bounds[1] - r - self.density))
+			dotBoxes = [[x, y, r, a]]
 
+		#now, if we have multiple ns we are trying to use for an overlay, let's do that...
+		if type(ns) == list and len(ns) > 1:
+			newBoxes = []
+			print sepDots
+			print dotBoxes
+	
+			for d in dotBoxes:
+				a = d[3]
+				for sp in sepDots:
+					if a in sp:
+						index = sepDots.index(sp)
+						d.append(index)
+						newBoxes.append(d)
+			self.dotBoxes = newBoxes
 
-	def MultiArranger(self, ns):
-		dotAreas = []
-		areaList = []
-		for n in ns:
-			areas = self.dotSolver(n)
-			dotAreas.append(areas)
-			areaList += areas
-
-		dotboxes = self.dotArranger(areaList)
-
-		newBoxes = []
-
-		for d in dotBoxes:
-			a = d[3]
-			exit = False
-			count = 0
-			while not exit:			
-				i = count % len(areas)
-				if a in areas[i]:
-					index = areas.index(a)
-					newD = d + [i]
-					newBoxes.append(newD)
-					areas.pop(index)
-					exit = True
-				count+=1
-
-		return newBoxes
+		#otherwise just return the dots we've got
+		else:		
+			self.dotBoxes = dotBoxes
 
 	def drawSingle(self, dotBoxes):
 		pass
@@ -223,8 +228,21 @@ class DotMaster:
 	def drawDouble(self, dotBoxes1, dotBoxes2):
 		pass
 
-	def drawOverlay(self, dotBoxes):
-		pass
+	def drawOverlay(self, name, dpi=96):
+		#make a left/right dot array stimulus from two groups of bounding boxes
+		image = Image.new("RGB", self.box, self.bgcolor)
+
+		draw = ImageDraw.Draw(image)
+
+		for d in self.dotBoxes:
+			#draw the dots on the left
+			box1 = [d[0] - d[2], d[1] - d[2], d[0] + d[2], d[1] + d[2]]
+			draw.ellipse(box1, fill = self.colors[d[4]])
+
+		del draw
+
+		image.save("%s_OL_NS.bmp" % name, "BMP", dpi=dpi)
+		
 
 bgcolor = (132, 130, 132)
 colors = [(255, 255, 16), (0, 4, 214)]
@@ -232,10 +250,12 @@ box = (480, 720) #region of screen occupied by dots
 #areas = [0.05, 0.1, 0.15, 0.2, 0.25] #area of box taken up by dots
 area = 0.05
 
-ns = [5, 8]
+nss = [[5, 8], [3,9], [10, 20], [2, 3]]
 
-dotMaster = DotMaster(box, area)
+dotMaster = DotMaster(box, area, colors = colors, bgcolor = bgcolor)
 
-newBoxes = dotMaster.MultiArranger(ns)
+for ns in nss:
+	dotMaster.dotArranger(ns)
 
-print newBoxes
+	dotMaster.drawOverlay(str(ns))
+
