276603905a6bb05f423077b8895626e21b1aab19
cbattista <battista.christian@gmail.com>
2010-11-10 14:48:03 -0500

    basic typo and bug fixes

--- Dots/dots.py
+++ Dots/dots.py
@@ -3,6 +3,8 @@
 import sys
 import os
 import pickle
+from VisionEgg.Textures import *
+from VisionEgg.Core import *
 
 sys.path.append("c:\code\OpenPsyc")
 
@@ -10,6 +12,7 @@ import experiments
 import subject
 import shuffler
 import CBalance
+
 
 blocks = ["sequential", "paired", "overlapping"]
 
@@ -22,11 +25,19 @@ else:
 
 blockOrder = cb.advance()
 
+f = open("cb.pck", "w")
+pickle.dump(cb, f)
+f.close()
+
+stimLib = "stimLib"
+
+screen = get_default_screen()
+
 for block in blockOrder:
 
 	ratios = shuffler.Condition([.3, .4, .5, .6, .7, .8], "ratio", 6)
 	seeds = shuffler.Condition([4, 5, 6, 7, 8], "seed", 6)
-	size = shuffler.Condition(["control", "noncontrol"], "size", 5)
+	size = shuffler.Condition(["SC", "NSC"], "size", 5)
 
 	order = ["large", "small"]
 	trials = 240
@@ -45,28 +56,30 @@ for block in blockOrder:
 		
 		exemplar = 1
 		
-		if block == "overlay":
-			fname = "%s_%s_%s_OL_NS_%s.bmp" % (ratio, n1, n2, exemplar)
+		if block == "overlapping":
+			fname = "%s_%s_%s_%s_OL_%s.bmp" % (ratio, n1, n2, exemplar, size)
 			
-			t = Texture(Image.open(fname))
+			print fname
+			
+			t = Texture(Image.open(os.path.join(stimLib, fname)))
 			x = screen.size[0] / 2
 			y = screen.size[1] / 2
 			s = TextureStimulus(texture = t, position = (x, y), anchor = 'center')
 			v = Viewport(screen=screen, stimuli=[s])
-			p = Presentation(go_duration = (0, 'seconds'), viewports=[v])
+			p = Presentation(go_duration = (0.5, 'seconds'), viewports=[v])
 			p.go()
 		
 		else:
 			if order == "large":
-				fname1 = "%s_%s_%s_S2_%s.bmp" % (ratio, n1, n2, exemplar)
-				fname2 = "%s_%s_%s_S1_%s.bmp" % (ratio, n1, n2, exemplar)
+				fname1 = "%s_%s_%s_%s_S2_%s.bmp" % (ratio, n1, n2, exemplar, size)
+				fname2 = "%s_%s_%s_%s_S1_%s.bmp" % (ratio, n1, n2, exemplar, size)
 			else:
-				fname1 = "%s_%s_%s_S1_%s.bmp" % (ratio, n1, n2, exemplar)
-				fname2 = "%s_%s_%s_S2_%s.bmp" % (ratio, n1, n2, exemplar)
+				fname1 = "%s_%s_%s_%s_S1_%s.bmp" % (ratio, n1, n2, exemplar, size)
+				fname2 = "%s_%s_%s_%s_S2_%s.bmp" % (ratio, n1, n2, exemplar, size)
 				
 			####
-			t1 = Texture(Image.open(fname1))
-			t2 = Texture(Image.open(fname2))
+			t1 = Texture(Image.open(os.path.join(stimLib,fname1)))
+			t2 = Texture(Image.open(os.path.join(stimLib,fname2)))
 
 			if block == "sequential":
 				x = screen.size[0] / 2
@@ -88,9 +101,9 @@ for block in blockOrder:
 				x = screen.size[0] / 4
 				y = screen.size[1] / 2
 				
-				s1 = TextureStimulus(texture = tex, position = (x, y), anchor = 'center')
-				s2 = TextureStimulus(texture = tex2, position = (x * 3, y), anchor = 'center')	
+				s1 = TextureStimulus(texture = t1, position = (x, y), anchor = 'center')
+				s2 = TextureStimulus(texture = t2, position = (x * 3, y), anchor = 'center')	
 
-				v = viewport(screen=screen, stimuli=[s1,s2]
+				v = Viewport(screen=screen, stimuli=[s1,s2])
 				p = Presentation(go_duration=(0.5, 'seconds'), viewports=[v])
 				p.go()
