2b27980347b597f673782d7f2ab77d0d998ff910
cogdev <cogdev@6232ac.ssc.uwo.ca>
2011-01-19 17:10:02 -0500

    improvements to the printText function, now centers text horizontally and vertically

--- experiments.py
+++ experiments.py
@@ -26,11 +26,13 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #we ask for the subject #, the session #, the subject's sex and their handedness
 
 import pygame
+import Image
 import VisionEgg
 from VisionEgg.GUI import *
 from VisionEgg.Core import *
 from VisionEgg.FlowControl import Presentation
 from VisionEgg.Text import *
+from VisionEgg.Textures import *
 
 
 
@@ -38,77 +40,73 @@ from VisionEgg.Text import *
 class Point:
 	pass
 
-def printWord (screen, theText, fontSize, theColor, h_anchor = 1, v_anchor = 1):
-	x = (screen.get_size()[0] / 4) * h_anchor
-	y = screen.get_size()[1] / 2 * v_anchor
+def printWord (screen, theText, fontSize, theColor):
+	x = (screen.get_size()[0] / 2)
+	y = screen.get_size()[1] / 2
 	myFont = pygame.font.Font(None, fontSize)
-	x = x - (myFont.size(theText)[1])
+	x = x - (myFont.size(theText)[0] / 2)
+	y = y - (myFont.size(theText)[1] / 2)
 	word = Text(text = theText, position = (x,y), color = theColor, font_size = fontSize)
 	viewport = Viewport (screen = screen, stimuli = [word])
 	return word, viewport
 
-def printText (screen, theText, fontSize, theColor, h_anchor = 1, v_anchor = 1):
+def printText (screen, theText, fontSize, theColor):
 	instructions = []
 	textColor = theColor
 	screensize = screen.get_size()
 	spacebuff = Point()
-	spacebuff.x = screensize[0]/12 * h_anchor
-	spacebuff.y = screensize[1] - (screensize[1]/12 * v_anchor)
+	spacebuff.x = screensize[0]/12
+	spacebuff.y = screensize[1] - (screensize[1]/12)
 	myFont = pygame.font.Font(None, fontSize)
+	fittedText = []
 	for line in theText:
-		if line == "\n":
-			spacebuff.y = spacebuff.y - myFont.size('A')[1]*2
-		else:
-			myLine = line.replace("\n", "")
-			myWords = myLine.split(' ')
-			rangeCount = 1
-			doesItFit = ""
-			n = len(myWords)
-			while n >= 0:
+		myWords = line.split(' ')
+		doesItFit = ""
+		while len(myWords):
+			word = myWords.pop(0)
+			itFits = doesItFit
+			doesItFit += word + (" ")
+			fitVal = screensize[0] - spacebuff.x * 2
+			if myFont.size(doesItFit)[0] < fitVal:
 				itFits = doesItFit
-				doesItFit = doesItFit + myWords[rangeCount-1] + (" ")
-				fitVal = screensize[0] - spacebuff.x * 2
-				if myFont.size(doesItFit)[0] < fitVal:
-					if n == rangeCount:
-						spacebuff.y = spacebuff.y - myFont.size('A')[1]
-						instructions.append(Text ( text = doesItFit, position = (spacebuff.x, spacebuff.y), color = theColor, font_size = fontSize))
-						n = 0
-						doesItFit = ""
-						break
-					else:
-						rangeCount = rangeCount + 1
-						itFits = doesItFit
-						
-				else:
-					spacebuff.y = spacebuff.y - myFont.size('A')[1]
-					instructions.append(Text ( text = itFits, position = (spacebuff.x, spacebuff.y), color = theColor, font_size = fontSize))
-					doesItFit = ""
-					n = n -rangeCount + 1
-					del myWords[0:rangeCount-1]
-					rangeCount = 1
+					
+			else:
+				fittedText.append(doesItFit)
+				doesItFit = ""
+		fittedText.append(itFits)
+
+	xpos = screen.size[0] / 2
+	ypos = screen.size[1] / 2 + len(fittedText) / 2 * myFont.size(fittedText[0])[1]
+
+
+	for line in fittedText:
+		
+		instructions.append(Text 
+							(text = line, 
+							anchor = 'center', 
+							position = (xpos, ypos - fittedText.index(line) * myFont.size(line)[1]), 
+							color = theColor, 
+							font_size = fontSize)
+							)
+
+
+
 	viewport = Viewport( screen=screen, stimuli=instructions)
 	return instructions, viewport
 
-	f = open(script, 'r')
-	
-	trialLines = []
-	
-	for line in f.readlines():
-		trialLines.append(line)
-	
-	return trialLines
 		
 #this function blits some parsed text on the screen
 #it takes the screen to blit to as an argument and also the file which holds the text
 #since it just shows text and then quits once SPACEBAR is pressed, it is good for showing experiment instructions, hence the name
-def showInstructions(screen,  text, textSize=30, textcolor=(0, 0, 0)):
+def showInstructions(screen,  text, textSize=60, textcolor=(255, 255, 255)):
 	#create our instruction screen
-	insText = text.split('\n')
+	#load up the instruction text
+	insText = text.split("\n")
 
 	if len(insText) > 1:
 		insStim, insView = printText(screen, insText, textSize, textcolor)
 	else:
-		insSim, insView = printWord(screen, insText, textSize, textcolor)
+		insSim, insView = printWord(screen, insText[0], textSize, textcolor)
 
 	instructions = Presentation(go_duration=('forever',), viewports=[insView])
 
@@ -124,4 +122,22 @@ def showInstructions(screen,  text, textSize=30, textcolor=(0, 0, 0)):
 
 	#show instructions
 	instructions.go()
+
+def showImage(screen, img, duration=1):
+	img = Image.open(img)
+
+	#create textures
+	tex = Texture(img)
+
+	x = screen.size[0] / 2
+	y = screen.size[1] / 2
+
+	stimulus = TextureStimulus(texture = tex, position = (x, y), anchor = 'center')
+
+	#create viewports
+	viewport = Viewport(screen=screen, stimuli=[stimulus])
+
+	image = Presentation(go_duration=(duration, 'seconds'), viewports=[viewport])
+	image.go()
+
 
