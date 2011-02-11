103dd2cd57e827cc6af53ff6d9aae25779271507
christian <battista.christian@gmail.com>
2011-02-10 19:48:18 -0500

    make the shape stimuli for ShapeCon

--- ShapeCon/ShapeMaker.py
+++ ShapeCon/ShapeMaker.py
@@ -8,32 +8,33 @@ ratios = [.9, .75, .66, .5, .33]
 seeds = [6, 7, 8, 9,10, 11]
 reps = [1,2,3,4,5,6]
 
+area = 0.03
 
 for shape in ['square', 'triangle']:
-	if shape == 'square':
-		perimeter = 0.8
-		area = 0.045
-	elif shape == 'triangle':
-		perimeter = 1.6
-		area = 0.045
-	for sm, smm in zip(['area', 'perimeter'], [area, perimeter]):
-		for r in ratios:
+	for r in ratios:
 
-			if sm == 'area':
-				control = 'perimeter'
-			else:
-				control = 'area'
+		shapeMaster = ShapeMaster(box, [area/r, area], shape=shape, sizemeasure = 'area', colors = colors, bgcolor = bgcolor, control='perimeter')
+			
+		for s in seeds:
+			for rep in reps:
+				n1 = s
+				n2 = int(round((s * 1/r), 0))
+				shapeMaster.shapeArranger([n1, n2])
 
-			shapeMaster = ShapeMaster(box, [smm/r, smm], shape=shape, sizemeasure = sm, colors = colors, bgcolor = bgcolor, control=control)
+				name = "%s_%s_%s_%s_incon" % (r, n1, n2, rep)		
+				shapeMaster.drawSingle(name)
 
-			for s in seeds:
-				print r, s
-				for rep in reps:
-					n1 = s
-					n2 = int(round((s * 1/r), 0))
-					shapeMaster.shapeArranger([n1, n2])
+for shape in ['square', 'triangle']:
+	for r in ratios:
 
-					name = "%s_%s_%s_%s_%s" % (sm, r, n1, n2, rep)		
-					shapeMaster.drawSingle(name)
+		shapeMaster = ShapeMaster(box, [area, area], shape=shape, sizemeasure = 'area', colors = colors, bgcolor = bgcolor)
+			
+		for s in seeds:
+			for rep in reps:
+				n1 = s
+				n2 = int(round((s * 1/r), 0))
+				shapeMaster.shapeArranger([n1, n2])
 
+				name = "%s_%s_%s_%s_con" % (r, n1, n2, rep)		
+				shapeMaster.drawSingle(name)
 
