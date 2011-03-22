f6542f497c57f15b5ee9b68bfa56e8c52e509885
christian <battista.christian@gmail.com>
2011-03-22 11:03:35 -0400

    support for drawing single dots now added

--- stimCreators/ShapeMaster.py
+++ stimCreators/ShapeMaster.py
@@ -6,7 +6,7 @@ import copy
 from euclid import euclid
 		   
 class ShapeMaster:
-	def __init__(self, box, shapesize, shape= 'circle', sizemeasure='area', sizectrl = 'SC', density=5, separation=10, colors =[[255, 255, 255]], bgcolor = [0,0,0], control='', logFile = "dot_log.csv"):
+	def __init__(self, box, shapesize, shape= 'circle', sizemeasure='area', sizectrl = 'SC', density=5, separation=20, colors =[[255, 255, 255]], overlay = False, bgcolor = [0,0,0], control='', logFile = "dot_log.csv"):
 		self.box = box
 		self.logFile = logFile
 		self.ctl_iters = 1
@@ -19,7 +19,8 @@ class ShapeMaster:
 				self.shapesize.append(box[0] * box[1] * d)
 			elif sizemeasure == 'perimeter':
 				self.shapesize.append((box[0] + box[1]) * d)
-			
+		
+		self.overlay = overlay
 		self.sizemeasure = sizemeasure
 		self.density = density
 		self.separation = separation
@@ -32,7 +33,7 @@ class ShapeMaster:
 	def shapeSolver(self, n, size, MIN=.2, MAX=.8, control = ''):
 
 		#input - size, number of dots
-		#output - radius of each dot
+		#stimuli - radius of each dot
 		#solver algo
 		#1 - calculate average size
 		#2 - generate a random value which is a portion of that size
@@ -44,24 +45,27 @@ class ShapeMaster:
 		avg = size / float(n)
 		operations = [-1, 1]
 		mySizes = []
-		
-		#make a guess on the average sizes of the n-1 of the items
-		for i in range(n-1):
-			num = random.uniform(MIN, MAX)
-			operation = random.choice(operations)
-			mySizes.append(avg + (operation * num * avg))
 
-		#determine the appropriate size of the nth item
-		total = sum(mySizes)
-		diff = size - total
+		if n > 1:
+			#make a guess on the average sizes of the n-1 of the items
+			for i in range(n-1):
+				num = random.uniform(MIN, MAX)
+				operation = random.choice(operations)
+				mySizes.append(avg + (operation * num * avg))
+
+			#determine the appropriate size of the nth item
+			total = sum(mySizes)
+			diff = size - total
 		
-		if diff > 0 and diff >= (avg*MIN) and diff <= (avg*MAX):
-			mySizes.append(diff)
+			if diff > 0 and diff >= (avg*MIN) and diff <= (avg*MAX):
+				mySizes.append(diff)
+			else:
+				mySizes = []
+				while not mySizes:
+					mySizes = self.shapeSolver(n, size)
+				mySizes = mySizes[0]
 		else:
-			mySizes = []
-			while not mySizes:
-				mySizes = self.shapeSolver(n, size)
-			mySizes = mySizes[0]
+			mySizes = [size]
 		
 		
 		#optional step, control for another size dimension
@@ -93,15 +97,16 @@ class ShapeMaster:
 				return []
 			#controlvalue has been set
 			else:
-				
 				#% similarity of controlled value
-				threshold = 85
+				threshold = 95
+				#print self.controlValue, controlSizes
 				vals = [self.controlValue, sum(controlSizes)]
 				control_ratio =  min(vals) / float(max(vals)) * 100.
 				#check and see whether the control dimension is controlled enough (95% threshold)
 				#print control_ratio
-				print self.ctl_iters
-				if control_ratio >= threshold or self.ctl_iters <= 1000:
+				#print self.ctl_iters
+				#if control_ratio >= threshold or self.ctl_iters <= 1000:
+				if control_ratio >= threshold or self.ctl_iters >= 10000:
 					#it is, so we're done and we can reset the control value
 					self.ctl_iters = 1
 					return mySizes, sum(controlSizes)
@@ -127,51 +132,62 @@ class ShapeMaster:
 			areaSums = []
 			periSums = []
 
-			for n, area in zip(ns, self.shapesize):
-				areas = []
-				while not areas:
-					areas = self.shapeSolver(n, area, control=control)
-				areas, controlSize = areas
 
-				perims = map(lambda(x) : euclid[self.shape]['perimeter'](euclid[self.shape]['radius'](x, 'area')), areas)
-				areaSums.append(int(sum(areas)))
-				periSums.append(int(sum(perims)))
-		
-				sepShapes.append(areas)
-				sizeList += areas
+			for n, size in zip(ns, self.shapesize):
+				sizes = []
+				while not sizes:
+					sizes, controlSize = self.shapeSolver(n, size, control=control)
+
+				if self.sizemeasure == 'area':
+					areaSums.append(int(sum(sizes)))
+					perims = map(lambda(x) : euclid[self.shape]['perimeter'](euclid[self.shape]['radius'](x, 'area')), sizes)
+					periSums.append(int(sum(perims)))
+				elif self.sizemeasure == 'perimeter':
+					periSums.append(int(sum(sizes)))
+					areas = map(lambda(x) : euclid[self.shape]['area'](euclid[self.shape]['radius'](x, 'perimeter')), sizes) 
+					areaSums.append(int(sum(areas)))
+	
+				sepShapes.append(sizes)
+				sizeList += sizes
+
 
 			self.areaSums = areaSums
 			self.periSums = periSums
-			#area_r = float(areaSums[0]) / areaSums[1]
-			#peri_r = float(periSums[0]) / periSums[1]
+			area_r = float(areaSums[0]) / areaSums[1]
+			peri_r = float(periSums[0]) / periSums[1]
 
-			self.size_log = "%s, %s, %s" % (self.shape, areaSums, periSums)
+
+			self.size_log = "%s, %s, %s, %s, %s, %s" % (areaSums[0], areaSums[1], round(area_r, 2), periSums[0], periSums[1], round(peri_r, 2))
 	
 		else:
 			print "Just what the hell do you think you're doing"
 
-		#print "sizelist %s, sepShapes %s" % (sizeList, sepShapes)
 		self.controlValue = False
+		print sizeList, sepShapes
 		return sizeList, sepShapes
 
-	def shapeArranger(self, ns, n1, n2, r):
+	def shapeArranger(self, ns):
 		#1 - place a dot box in a random location which does not overlap the edges
 		#2 - place a dot in a random location which does not overlap the edges or any other dot boxes
 		#3 - repeat until no dots are left
 
 		goodList = 0
 		breaks = 0
+		
+		ratio = float(ns[0]) / float(ns[1])
 
-		self.ratio_log = "%s, %s, %s, %s" % (n1, n2, r, round(1/r, 2))
+		self.ratio_log = "%s, %s, %s, %s" % (ns[0], ns[1], round(ratio, 2), round(1/ratio, 2))
+
 
 		sizeList, sepShapes = self.generateLists(ns, self.control)
 		
-		if len(sizeList) != 1:
+		if len(sizeList) > 1:
 
 			while not goodList:
 				shapeBoxes = []
 				count = 1
 				shapesizes = copy.deepcopy(sizeList)
+
 				while len(shapesizes):
 					a = shapesizes[-1]
 					r = int(euclid[self.shape]['radius'](a, self.sizemeasure))
@@ -238,6 +254,7 @@ class ShapeMaster:
 			x = int(random.uniform(r + self.density, self.bounds[0] - r - self.density))
 			y = int(random.uniform(r + self.density, self.bounds[1] - r - self.density))
 			shapeBoxes = [[x, y, r, a]]
+			print shapeBoxes
 
 		#now, if we have multiple ns we are trying to use for an overlay, let's do that...
 		if type(ns) == list and len(ns) > 1:
@@ -258,47 +275,65 @@ class ShapeMaster:
 
 	def drawSingle(self, name, dpi=96):
 		#obtain all the possible colors
-			
-		c = self.colors
-
-		image = Image.new("RGB", self.box, self.bgcolor)
-				
-		draw = ImageDraw.Draw(image)
+		cols = []
 		for d in self.shapeBoxes:
-			box1 = [d[0] - d[2], d[1] - d[2], d[0] + d[2], d[1] + d[2]]
-			if self.shape == 'circle':
-				draw.ellipse(box1, fill = c)
-			elif self.shape == 'square':
-				draw.rectangle(box1, fill = c)
-			elif self.shape == 'triangle':
-				draw.polygon([box1[0], box1[1], box1[0], box1[3], box1[2], box1[3]], fill = c)
-						
-					
-		del draw
-				
-		fname = "%s_%s.bmp" % (self.shape, name)
-
-		image.save("output/%s" % fname, "BMP", dpi=dpi)
+		
+			cols.append(d[4])
 			
-		self.printLog(fname)
+		cols = set(cols)
+		cols = list(cols)
+
+		count = 1
+
+		for c in cols:
+			image = Image.new("RGB", self.box, self.bgcolor)
+					
+			draw = ImageDraw.Draw(image)
+			for d in self.shapeBoxes:
+				if d[4] == c:
+					box1 = [d[0] - d[2], d[1] - d[2], d[0] + d[2], d[1] + d[2]]
+					if self.shape == 'circle':
+						draw.ellipse(box1, fill = self.colors[c])
+					elif self.shape == 'square':
+						draw.rectangle(box1, fill = self.colors[c])
+					elif self.shape == 'triangle':
+						draw.polygon([box1[0], box1[1], box1[0], box1[3], box1[2], box1[3]], fill = self.colors[c])
+
+					
+			del draw
+				
+			fname = "%s_%s_S%s.bmp" % (self.shape, name, count)
+
+			image.save("stimuli/%s" % fname, "BMP", dpi=dpi)
+				
+			count+=1
+			self.printLog(fname)
+
+
 
 	def drawOverlay(self, name, dpi=96):
 		#make a left/right dot array stimulus from two groups of bounding boxes
-		image = Image.new("RGB", self.box, self.bgcolor)
 
-		draw = ImageDraw.Draw(image)
+		if self.overlay:
 
-		for d in self.shapeBoxes:
-			#draw the dots on the left
-			box1 = [d[0] - d[2], d[1] - d[2], d[0] + d[2], d[1] + d[2]]
-			draw.ellipse(box1, fill = self.colors[d[4]])
+			image = Image.new("RGB", self.box, self.bgcolor)
 
-		del draw
+			draw = ImageDraw.Draw(image)
 
-		fname = "%s_OL.bmp" % (name)
+			for d in self.shapeBoxes:
+				#draw the dots on the left
+				box1 = [d[0] - d[2], d[1] - d[2], d[0] + d[2], d[1] + d[2]]
+				draw.ellipse(box1, fill = self.colors[d[4]])
 
-		image.save("output/%s" % fname, "BMP", dpi=dpi)
-		self.printLog(fname)		
+			del draw
+
+			fname = "%s_OL.bmp" % (name)
+
+			image.save("stimuli/%s" % fname, "BMP", dpi=dpi)
+			self.printLog(fname)		
+
+		else:
+			raise Exception("You told me you didn't want an overlay, sucka!  You need to pass in overlay=True in the arguments or I will not be able to draw things right!")
 
 
 	def printLog(self, fname):
@@ -306,9 +341,9 @@ class ShapeMaster:
 			f = open(self.logFile, "a")
 		else:
 			f = open(self.logFile, "w")
-			f.write("file,n1,n2,ratio,1/ratio,shape,area,per\n")
+			f.write("file,shape,n1,n2,ratio,1/ratio,area_n1,area_n2,area_ratio,per_n1,per_n2,per_ratio\n")
 
-		log = "%s, %s, %s" % (fname, self.ratio_log, self.size_log)
+		log = "%s, %s, %s, %s" % (fname, self.shape, self.ratio_log, self.size_log)
 
 		f.write(log + "\n")
 		f.close()
