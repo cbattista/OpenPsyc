aa247725cd562b238dba9a527578005584596e08
christian <battista.christian@gmail.com>
2011-03-31 16:25:36 -0400

    file to test out the performance of the guiMaster system

---
+++ GMTest.py
@@ -0,0 +1,27 @@
+#GMTest.py
+
+#testing out the guiMaster on a Shuffler object
+
+import guiMaster
+
+class TestObj:
+	def __init__(self, items = ['1', '2', '3', [1,2,3,4]], number = 1.23, wang = 1, chung=False, chong={'wee' : 'nis', 'pen' : 15}):
+		self.items = items
+		self.number = number
+		self.wang = wang
+
+	def testFunc(self, testarg=False):
+		print self.items
+		print self.number
+		print self.wang
+
+		print testarg
+
+	def testFunc2(self, testarg=True):
+		print self.chung
+		print self.chong
+		print testarg
+
+gm = guiMaster.objApp(TestObj)
+
+gm.go()
