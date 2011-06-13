5c4223dc0212fa69d2bb614e93a9fe4a7a981651
cogdev <cogdev@6232ac.ssc.uwo.ca>
2011-06-13 13:55:32 -0400

    changed 'classification' to 'kind' to match with problem.py

--- CAT/testProblem.py
+++ CAT/testProblem.py
@@ -40,9 +40,9 @@ class TestProblems(unittest.TestCase):
 	def test_distinct(self):
 		for p in problems:
 			self.Problems.append(p)
-		items = self.Problems.distinct('classification')
+		items = self.Problems.distinct('kind')
 		self.assertTrue(type(items) == list)
-		items = self.Problems.distinct('classification', {'strat': 'mem'})
+		items = self.Problems.distinct('kind', {'strat': 'mem'})
 		self.assertTrue(type(items) == list)
 
 	def test_classify(self):
@@ -50,18 +50,18 @@ class TestProblems(unittest.TestCase):
 		p1 = problem.Problem([3,4], {'trial':1, 'strat': 'mem', 'ACC':1, 'RT': 400, 'misfire':m})
 		p2 = problem.Problem([4,3], {'trial':2, 'strat': 'mem', 'ACC':1, 'RT': 450, 'misfire':m})
 		self.Problems.append(p1)
-		c = self.Problems.distinct('classification')[0]
+		c = self.Problems.distinct('kind')[0]
 		self.assertTrue(c == 'temp')
 		self.Problems.append(p2)
-		c = self.Problems.distinct('classification')[0]
+		c = self.Problems.distinct('kind')[0]
 		self.assertTrue(c == 'verified')
 		p3 = problem.Problem([3,4], {'trial':3, 'strat': 'calc', 'ACC': 1, 'RT': 500, 'misfire':m})
 		self.Problems.append(p3)
-		c = self.Problems.distinct('classification')[0]
+		c = self.Problems.distinct('kind')[0]
 		self.assertTrue(c == 'erratic')
 		p4 = problem.Problem([3,4], {'trial':3, 'strat': 'calc', 'ACC': 0, 'RT': 500, 'misfire':m})
 		self.Problems.append(p4)
-		c = self.Problems.distinct('classification')[0]
+		c = self.Problems.distinct('kind')[0]
 		self.assertTrue(c == 'incorrect')
 
 		
