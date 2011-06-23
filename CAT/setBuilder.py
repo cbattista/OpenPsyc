3aca65de4206d271161aa5c2690a2006c9196d3d
christian <battista.christian@gmail.com>
2011-06-23 17:58:28 -0400

    a new training set builder that uses the problems class

---
+++ CAT/setBuilder.py
@@ -0,0 +1,38 @@
+import mongoTools
+
+class TrainingBuilder:
+	def __init__(self, problems, setSize=20):
+		self.problems = problems
+		self.setSize = setSize
+
+		self.f = open("P_%s.csv" % self.problems.sid)
+		self.build('mem')
+		self.build('calc')
+		self.f.close()
+
+	def build(self, strat):
+		rows = self.problems.query({'strat': strat, 'kind': 'verified'})
+
+		rows = rows.sort({'solution' : 1})
+
+		trainingProbs = []
+
+		#want to come up with a roughly balanced set of solution sizes (trained vs untrained)
+		count = 1
+		for row in rows:
+			#every other problem should be added to the list
+			if count % 2:
+				trainingProbs.append(row)
+				row['trained'] = 'trained'
+			else:
+				row['trained'] = 'novel'
+
+			self.problems.posts.save(row)
+
+			count += 1	
+
+		for p in trainingProbs[:self.setSize]:
+			line = "%s, %s, %s, %s, %s\n" % (problems.sid, p['n1'], p['n2'], p['strat'], p['solution']) 
+			self.f.write(line)
+
+
