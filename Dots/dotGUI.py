96895258153d048c89af3043da82caa159ae96a9
samar <samar@cdlsamar.(none)>
2011-08-05 14:17:20 -0400

    added listbox with response format and prettied up the sizer a bit also changed subject id to string, not int

--- Dots/dotGUI.py
+++ Dots/dotGUI.py
@@ -17,26 +17,36 @@ class dotConfig(wx.Frame):
 	def __init__(self, *args, **kwargs):
 		wx.Frame.__init__(self, *args, **kwargs)
 
-		sizer = wx.BoxSizer(wx.VERTICAL)
+		sizer = wx.GridSizer(4, 2, 10, 10)
 
 		controls = {}
-		controls['subject id'] = 666
+		controls['subject id'] = 'practice'
 		controls['trials'] = 240
 		controls['dot duration'] = 750
 		controls['mask duration'] = 500
 		controls['ISI'] = 750
 		controls['break trial'] = 60
+		controls['subtrials'] = 0
+		controls['response'] = "touch"
 
 		self.controls = controls
 
 		self.ctrls = []
 
 		for k in self.controls.keys():
-			control = wx.SpinCtrl(self, -1, max=10000, initial=controls[k], size=(60, 80))
+			if k == 'subject id':
+				control = wx.TextCtrl(self, -1, controls[k], size=(60, 70))
+			elif k == 'response':
+				control = wx.ListBox(self, -1, choices=['touch', 'voice'])
+				control.SetStringSelection(controls[k])
+			else:
+				control = wx.SpinCtrl(self, -1, max=10000, initial=controls[k], size=(60, 70))
 			control.label = k
 			self.ctrls.append(control)
-			sizer.Add(wx.StaticText(self, -1, k))
-			sizer.Add(control)
+			mini = wx.BoxSizer(wx.VERTICAL)
+			mini.Add(wx.StaticText(self, -1, k))
+			mini.Add(control)
+			sizer.Add(mini)
 
 		sizer.Add(wx.Button(self, -1, 'Proceed to Experiment'))
 
@@ -46,7 +56,12 @@ class dotConfig(wx.Frame):
 
 	def onButton(self, event):
 		for c in self.ctrls:
-			self.controls[c.label] = int(c.GetValue())
+			if c.label == 'subject id':
+				self.controls[c.label] = c.GetValue()
+			elif c.label == 'response':
+				self.controls[c.label] = c.GetStringSelection()
+			else:
+				self.controls[c.label] = int(c.GetValue())
 
 		f = open("controls.pck", "w")
 		pickle.dump(self.controls, f)
