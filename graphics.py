24a74fd47db438cca8bde4203bfc701e8e9e20aa
christian <battista.christian@gmail.com>
2011-12-21 02:08:24 -0500

    better random colour selection, avoiding overly light colours

--- graphics.py
+++ graphics.py
@@ -8,10 +8,24 @@ class colorDict(dict):
 		if self.d.has_key(key):
 			return self.d[key]
 		else:
-			c1 = random.randint(0, 10) / 10.
-			c2 = random.randint(0, 10) / 10.
-			c3 = random.randint(0, 10) / 10.
+			c1 = random.randint(1, 5) / 10.
+			c2 = random.randint(1, 5) / 10.
+			c3 = random.randint(1, 5) / 10.
+
+			amplify = random.choice([0, 1, 2])
+
+			color = [c1, c2, c3]
+			
+			newcol = color[amplify] * 1.5
+			if newcol > 1.0:
+				newcol = 1.
+
+			color[amplify] = newcol
+
 			self.d[key] = [c1, c2, c3]
+			
+			
+
 			return self.d[key]
 	
 	def __setitem__(self, key, value):
